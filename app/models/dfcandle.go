0d53028c9e7e17a45baba2c6d5522dfdf34f4c8e
bonn1112 <64962800+bonn1112@users.noreply.github.com>
2020-09-02 10:58:18 +0900

    add algorithm

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -16,6 +16,9 @@ type DataFrameCandle struct {
 	Emas          []Ema          `json:"emas,omitempty"`
 	BBands        *BBands        `json:"bbands,omitempty"`
 	IchimokuCloud *IchimokuCloud `json:"ichimoku,omitempty"`
+	Rsi           *Rsi           `json:"rsi,omitempty"`
+	Macd          *Macd          `json:"macd,omitempty"`
+	Hvs           []Hv           `json:"hvs,omitempty"`
 }
 
 type Sma struct {
@@ -42,6 +45,25 @@ type IchimokuCloud struct {
 	SenkouA []float64 `json:"senkoua,omitempty"`
 	SenkouB []float64 `json:"senkoub,omitempty"`
 	Chikou  []float64 `json:"chikou,omitempty"`
+}
+
+type Rsi struct {
+	Period int       `json:"period,omitempty"`
+	Values []float64 `json:"values,omitempty"`
+}
+
+type Macd struct {
+	FastPeriod   int       `json:"fast_period,omitempty"`
+	SlowPeriod   int       `json:"slow_period,omitempty"`
+	SignalPeriod int       `json:"signal_period,omitempty"`
+	Macd         []float64 `json:"macd,omitempty"`
+	MacdSignal   []float64 `json:"macd_signal,omitempty"`
+	MacdHist     []float64 `json:"macd_hist,omitempty"`
+}
+
+type Hv struct {
+	Period int       `json:"period,omitempty"`
+	Values []float64 `json:"values,omitempty"`
 }
 
 func (df *DataFrameCandle) Times() []time.Time {
@@ -140,6 +162,44 @@ func (df *DataFrameCandle) AddIchimoku() bool {
 			SenkouB: senkouB,
 			Chikou:  chikou,
 		}
+		return true
+	}
+	return false
+}
+
+func (df *DataFrameCandle) AddRsi(period int) bool {
+	if len(df.Candles) > period {
+		df.Rsi = &Rsi{
+			Period: period,
+			Values: talib.Rsi(df.Closes(), period),
+		}
+		return true
+	}
+	return false
+}
+
+func (df *DataFrameCandle) AddMacd(inFastPeriod, inSlowPeriod, inSignalPeriod int) bool {
+	if len(df.Candles) > 1 {
+		outMACD, outMACDSignal, outMACDHist := talib.Macd(df.Closes(), inFastPeriod, inSlowPeriod, inSignalPeriod)
+		df.Macd = &Macd{
+			FastPeriod:   inFastPeriod,
+			SlowPeriod:   inSlowPeriod,
+			SignalPeriod: inSignalPeriod,
+			Macd:         outMACD,
+			MacdSignal:   outMACDSignal,
+			MacdHist:     outMACDHist,
+		}
+		return true
+	}
+	return false
+}
+
+func (df *DataFrameCandle) AddHv(period int) bool {
+	if len(df.Candles) >= period {
+		df.Hvs = append(df.Hvs, Hv{
+			Period: period,
+			Values: tradingalgo.Hv(df.Closes(), period),
+		})
 		return true
 	}
 	return false
