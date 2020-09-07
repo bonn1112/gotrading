72979a016befe94e93d27d2f9c038e38236ed8e4
bonn1112 <akutsu0521@gmail.com>
2020-09-07 22:29:40 +0900

    add function about choose 3 ind out of 5

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"gotrading/app/controllers"
+	"gotrading/app/models"
 	"gotrading/config"
 	"gotrading/utils"
 )
 
 func main() {
+	df, _ := models.GetAllCandle(config.Config.ProductCode, time.Minute, 365)
 	utils.LoggingSettings(config.Config.LogFile)
 	controllers.StreamIngestionData()
 	log.Println(controllers.StartWebServer())
